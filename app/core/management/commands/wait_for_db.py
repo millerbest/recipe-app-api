0c4378d653776b6882ed2484eff1f527dc507808
Yibing Wang <y.wang@hollandptc.nl>
2023-06-01 00:05:11 +0200

    update actions and docker-compose

--- app/core/management/commands/wait_for_db.py
+++ app/core/management/commands/wait_for_db.py
@@ -16,6 +16,7 @@ class Command(BaseCommand):
         while db_up is False:
             try:
                 self.check(databases=["default"])
+                db_up = True
             except (Psycopg2OpError, OperationalError):
                 self.stdout.write("Database unavailable, waiting 1 second...")
                 time.sleep(1)
