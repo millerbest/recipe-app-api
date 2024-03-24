d82b0d2aa41a76c6b7f519b5dea004eebdca7823
Yibing Wang <y.wang@hollandptc.nl>
2024-03-25 00:39:13 +0100

    working on ingredients

--- app/recipe/serializers.py
+++ app/recipe/serializers.py
@@ -2,23 +2,16 @@
 
 from rest_framework import serializers
 
-from core.models import Recipe, Tag
+from core.models import Recipe, Tag, Ingredient
 
 
-class RecipeSerializer(serializers.ModelSerializer):
-    """Serializer for recipes."""
+class IngredientSerializer(serializers.ModelSerializer):
+    """Serializer for ingredients"""
 
     class Meta:
-        model = Recipe
-        fields = ["id", "title", "time_minutes", "price", "link"]
+        model = Ingredient
+        fields = ["id", "name"]
         read_only_fields = ["id"]
-
-
-class RecipeDetailSerializer(RecipeSerializer):
-    """Serializer for recipe detail view"""
-
-    class Meta(RecipeSerializer.Meta):
-        fields = RecipeSerializer.Meta.fields + ["description"]
 
 
 class TagSerializer(serializers.ModelSerializer):
@@ -28,3 +21,71 @@ class TagSerializer(serializers.ModelSerializer):
         model = Tag
         fields = ["id", "name"]
         read_only_fields = ["id"]
+
+
+class RecipeSerializer(serializers.ModelSerializer):
+    """Serializer for recipes."""
+
+    # Tags will be a list of tag objects, thus many=True
+    # Nested serializer is read-only by default
+    tags = TagSerializer(many=True, required=False)
+    ingredients = IngredientSerializer(many=True, required=False)
+
+    class Meta:
+        model = Recipe
+        fields = ["id", "title", "time_minutes", "price", "link", "tags", "ingredients"]
+        read_only_fields = ["id"]
+
+    def _get_or_create_tags(self, tags, recipe):
+        """Handle getting or creating tags as needed."""
+        # Get the authenticated user.
+        # This is the way to get the authenticated user in the serializer
+        auth_user = self.context["request"].user
+        for tag in tags:
+            # get_or_create is a helper method of the model manager
+            # The tag here only has name, no user, that's why we need user=auth_user
+            tag_obj, created = Tag.objects.get_or_create(user=auth_user, **tag)
+            recipe.tags.add(tag_obj)
+
+    def _get_or_create_ingredients(self, ingredients, recipe):
+        """Handle getting or creating ingredients as needed"""
+        auth_user = self.context["request"].user
+        for ingredient in ingredients:
+            ingredient_obj, created = Ingredient.objects.get_or_create(
+                user=auth_user, **ingredient
+            )
+            recipe.ingredients.add(ingredient_obj)
+
+    def create(self, validated_data):
+        """Create a new recipe"""
+        # Get tags from the validated data and remove it from the validated data
+        tags = validated_data.pop("tags", [])
+        ingredients = validated_data.pop("ingredients", [])
+        # Create the recipe with validated date (without tags)
+        recipe = Recipe.objects.create(**validated_data)
+        self._get_or_create_tags(tags=tags, recipe=recipe)
+        self._get_or_create_ingredients(ingredients=ingredients, recipe=recipe)
+
+        return recipe
+
+    def update(self, instance, validated_data):
+        """Update recipe, the instance is the existing instance to be updated"""
+        # pop the tags from the validated data
+        tags = validated_data.pop("tags", None)
+        if tags is not None:
+            # Remove all tags from the recipe
+            instance.tags.clear()
+            self._get_or_create_tags(tags, instance)
+
+        for attr, value in validated_data.items():
+            setattr(instance, attr, value)
+
+        instance.save()
+        return instance
+
+
+class RecipeDetailSerializer(RecipeSerializer):
+    """Serializer for recipe detail view"""
+
+    class Meta(RecipeSerializer.Meta):
+        fields = RecipeSerializer.Meta.fields + ["description"]
--- app/recipe/urls.py
+++ app/recipe/urls.py
@@ -11,6 +11,7 @@ from recipe import views
 router = DefaultRouter()
 router.register("recipes", views.RecipeViewSet)
 router.register("tags", views.TagViewSet)
+router.register("ingredients", views.IngredientViewSet)
 # enable auto-generated urls dependent on the ViewSet (for CRUD)
 
 app_name = "recipe"  # used for reverse urls
